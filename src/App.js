b3ed4177c621a21bcfb34861267ce00d35759620
MujtabaJunaid <mujtabajunaid412@gmail.com>
2025-09-19 08:42:23 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Chatbot from "./components/Chatbot";
+import Chatbot from "./components/chatbot.js";
 import { Container } from "@mui/material";
 
 function App() {
